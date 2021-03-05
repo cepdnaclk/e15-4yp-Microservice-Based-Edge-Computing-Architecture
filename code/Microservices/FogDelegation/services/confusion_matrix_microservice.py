9c9858cdd0cb594cfe1df5e1903e7c7588771c5e
Sewwandi Nisansala <sewwanis@gmail.com>
2021-03-05 15:41:40 +0530

    Fog microservices after Delegation added

---
+++ code/Microservices/FogDelegation/services/confusion_matrix_microservice.py
@@ -0,0 +1,144 @@
+# Load libraries
+import csv
+
+import numpy as np
+
+from sklearn.metrics import confusion_matrix
+
+import requests
+from flask import Flask
+
+import json
+from json import JSONEncoder
+
+import time
+import datetime
+
+a = datetime.datetime.now()
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+app = Flask(__name__)
+
+fog2_ip_address = "192.168.1.110"
+time_get_ac_status_confusion = 0
+time_get_speed_confusion = 0
+
+
+def write_to_csv(fileName, data):
+    with open(fileName, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Data:", data])
+
+
+@app.route('/ac_control/confusion_matrix', methods=['GET'])
+def ac_status_confuion():
+    global time_get_ac_status_confusion
+    start_time = time.time()
+    ac_control_confusion_matrix_function()
+    time_get_ac_status_confusion = time.time() - start_time
+    print("--- ac control confusion_matrix %s seconds ---" % (time.time() - start_time))
+    write_to_csv('time_get_ac_status_confusion.csv', time_get_ac_status_confusion)
+    return "confusion matrix"
+    # return str(confusion_matrix_value)
+
+
+@app.route('/speed/confusion_matrix', methods=['GET'])
+def speed_confuion():
+    global time_get_speed_confusion
+    start_time = time.time()
+    speed_confusion_matrix_function()
+    time_get_speed_confusion = time.time() - start_time
+    print("---speed confusion_matrix %s seconds ---" % (time.time() - start_time))
+    write_to_csv('time_get_speed_confusion.csv', time_get_speed_confusion)
+    return "confusion matrix"
+    # return str(confusion_matrix_value)
+
+
+def get_ac_control_y_test_data():
+    try:
+        req = requests.get("http://" + fog2_ip_address + ":4001/ac_control/y_test")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_ac_control_predict_data():
+    try:
+        req = requests.get("http://localhost:4003/ac_control/predict")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_speed_y_test_data():
+    try:
+        req = requests.get("http://" + fog2_ip_address + ":4001/speed/y_test")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def get_speed_predict_data():
+    try:
+        req = requests.get("http://localhost:4201/speed/predict")
+        decodedArrays = json.loads(req.text)
+
+        finalNumpyArray = np.asarray(decodedArrays["array"])
+
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return finalNumpyArray
+
+
+def ac_control_confusion_matrix_function():
+    print('confusion_matrix: ')
+    y_test = get_ac_control_y_test_data()
+    predict_data = get_ac_control_predict_data()
+
+    print("y_test len", len(y_test))
+    print("predict len", len(predict_data))
+
+    confusion_matrix_value = confusion_matrix(y_test[:len(predict_data)], predict_data)
+    print(confusion_matrix_value)
+    # return confusion_matrix_value
+
+
+def speed_confusion_matrix_function():
+    print('confusion_matrix: ')
+
+    y_test = get_speed_y_test_data()
+    predict_data = get_speed_predict_data()
+
+    print("y_test len", len(y_test))
+    print("predict len", len(predict_data))
+
+    confusion_matrix_value = confusion_matrix(y_test[:len(predict_data)], predict_data)
+    print(confusion_matrix_value)
+    # return confusion_matrix_value
+
+
+b = datetime.datetime.now()
+print("Execution Time:")
+print(b-a)
+
+if __name__ == '__main__':
+    app.run(port=4004,host='0.0.0.0', debug=True)
